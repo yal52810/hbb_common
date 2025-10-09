346636ca4c65a9f282babfb31056b60435f0a247
yal52810 <luyingyi818@outlook.com>
2025-10-09 18:00:12 +0800

    改为自己的 ID/ 中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["149.104.0.224"];
+pub const RS_PUB_KEY: &str = "OVuDwQ1XjaURH57jGAQ9mmXuaH+nAaQX85aSvoxiET8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
