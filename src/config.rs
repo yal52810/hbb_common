18710928e46022facbf5855b3e304dd9e8bb4ac3
yal52810 <luyingyi818@outlook.com>
2025-10-17 15:44:32 +0800

    改为自己的 ID/ 中继服务器

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["149.104.0.224"];
-pub const RS_PUB_KEY: &str = "OVuDwQ1XjaURH57jGAQ9mmXuaH+nAaQX85aSvoxiET8=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["tcp.sealosbja.site:47649"];
+pub const RS_PUB_KEY: &str = "KkKzrC1qcuFVp1Kn7QSh21lqfac5ydszPp9oiMqmOnQ=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
